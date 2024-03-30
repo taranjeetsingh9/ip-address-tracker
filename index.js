8ca69becbf36cdf015a90ad72dfd86e6bf476b5a
Taranjeet Singh <sin12506@sheridancollege.ca>
2024-03-30 17:02:26 -0400

    add modularity to code & current IP address Fetch

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+/*=================================================================
+----------------------IP Address API ------------------------------
+==================================================================*/
+
 const API_Endpoint = "https://geo.ipify.org/api/v2/country,city";
 const API_key = "at_MvZqZLcERCM0uC1KcpjVtBJbFDZLk";
 
@@ -7,34 +11,63 @@ let ipLocation = document.querySelector(".ipLocation");
 let timeZone = document.querySelector(".timeZone");
 let iProvider = document.querySelector(".isps");
 
+// display IP info function
+async function displayIPinfo(ipValue = "") {
+  try {
+    let url = `${API_Endpoint}?apiKey=${API_key}&ipAddress=${ipValue}`;
+    const res = await fetch(url);
+
+    if (!res.ok) {
+      console.log("Failed to Fetch IP information");
+    }
+    const data = await res.json();
+    return data;
+  } catch (error) {
+    console.error("Error fetching IP information:", error);
+  }
+}
+
+// function to update information in frontend
+function updateInfo(data) {
+  const { ip, location, isp } = data;
+  const { region, city, postalCode, timezone } = location;
+  ipAddValue.innerText = ip;
+
+  ipLocation.innerText = `${region},${city},${postalCode}`;
+  timeZone.innerText = `UTC ${timezone}`;
+  iProvider.innerText = isp;
+}
+
 // Function to handle the click event on the button and fetch IP information
 button.addEventListener("click", async () => {
-  // Get the IP address value entered by the user
   let ipValue = document.querySelector(".enterIP").value;
 
   try {
-    let url = `${API_Endpoint}?apiKey=${API_key}&ipAddress=${ipValue}`;
-
-    // Fetch IP information from the API
-    let res = await fetch(url);
-
-    if (!res.ok) {
-      throw new Error("Failed to Fetch IP information");
-    }
-    let data = await res.json();
-    console.log(data);
-    let region = data.location.region;
-    let city = data.location.city;
-    let postalCode = data.location.postalCode;
-    let timeZones = data.location.timezone;
-    let ispValues = data.isp;
-
-    ipAddValue.innerText = data.ip;
-    ipLocation.innerText = `${region},${city},${postalCode}`;
-    timeZone.innerText = `UTC ${timeZones}`;
-    iProvider.innerText = `${ispValues}`;
+    const data = await displayIPinfo(ipValue);
+    updateInfo(data);
   } catch (error) {
     console.log("Error Fetching IP information: ", error.message);
   }
 });
 
+// Automatically fetch IP information when the page loads
+window.addEventListener("DOMContentLoaded", async () => {
+  try {
+    const data = await displayIPinfo("");
+    console.log(data);
+    updateInfo(data);
+  } catch (error) {
+    console.error("Error Fetching IP information: ", error.message);
+  }
+});
+
+/*======================================================
+-------------------MAP API -----------------------------
+======================================================*/
+
+/*======================================================
+Improvements after compeletion
+1) Make sure proper naming
+2) Coding Modularity
+3) Always look for proper error handling
+====================================================== */
